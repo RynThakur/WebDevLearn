af71c299abc3f97acd5a44635dfcf1341d86d168
Aryan Thakur <rynthakur@gmail.com>
2024-09-12 22:59:23 +0530

    commit on 12th sept

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -12,7 +12,7 @@ function sayHello(){
 async function main(){
     let value = await sayHello(); // thread waits here
     console.log(value);
-}
+} 
 // await can only be used inside and async fn
 main();
 console.log("this will get logged first") // this will get logged first cuz await is used in main
